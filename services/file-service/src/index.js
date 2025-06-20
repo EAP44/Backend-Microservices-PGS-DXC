83ca356748e440aedba9986e3c47cefdc1bdb001
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-06-20 22:39:53 +0100

    Update index.js

--- services/file-service/src/index.js
+++ services/file-service/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.listen(3000,()=>{
-  console.log("http://hocalhost:3030")
+  console.log("le serveur le demare sur le port 3030 > http://hocalhost:3030 < ")
 };
